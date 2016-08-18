315c66205b18f26c18b976842edd40850ee75c86
wilson.soto <wilson.soto@wisoto.usa.loc>
2016-08-18 12:36:56 -0500

    OK

---
+++ Estructuras/src/Diagnose/Functions.java
@@ -0,0 +1,35 @@
+package diagnose;
+
+/**
+ * 
+ * @author wilson.soto
+ * @since 28.01.16
+ *
+ */
+public interface Functions {
+	
+	/**
+	 * Power x and y
+	 * 
+	 * @param x double number
+	 * @param n int number
+	 * 
+	 */
+	public double power (double x, int n);
+
+	/**
+	 * Factorial
+	 * 
+	 * @param n natural number
+	 * 
+	 */
+	public int factorial (int n);
+	
+	/**
+	 * Sin x
+	 * 
+	 * @param x double number
+	 * 
+	 */
+	public double sin_x (double x);
+}
--- Estructuras/src/diagnose/Functions.java
+++
@@ -1,8 +0,0 @@
-package diagnose;
-
-public interface Functions {
-	
-	public double power (double x, int n);
-	public int factorial (int n);
-	public double sin_x (double x);
-}
