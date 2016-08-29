089f4511795f9460f297ace228c1b0dd15e768e1
wilson.soto <wilson.soto@wisoto.usa.loc>
2016-08-29 10:40:57 -0500

    OK

--- Estructuras/src/Graphs/LinearList.java
+++ Estructuras/src/Graphs/LinearList.java
@@ -9,42 +9,28 @@ package graphs;
 
 public interface LinearList
 {
-    /**
-     * Return true if the list is empty. false otherwise
-     */
+    //return true if the list is empty. false otherwise
     public boolean isEmpty();
     
-    /**
-     * Return the number of elements in the list 
-     */
+    //return the number of elements in the list
     public int size();
     
-    /**
-     * Return the indexth element of the list 
-     */
+    //return the indexth element of the list
     public Object get(int index);
-
-    /**
-     * Return the position of the first occurrence of x
-     * in the list, return -1 if x is not in the list 
-     */
+    
+    //return the position of the first occurrence of x
+    //in the list, return -1 if x is not in the list
     public int indexOf(Object theElement);
     
-    /**
-     * Remove and return the indexth element, elements
-     * with higher index have their index reduced by 1  
-     */
+    //remove and return the indexth element, elements
+    //with higher index have their index reduced by 1
     public Object remove(int index);
-
-    /**
-     * Insert x as the indexth element, elements with
-     * index index have their index increased by 1  
-     */
+    
+    //insert x as the indexth element, elements with
+    //index ≥ index have their index increased by 1
     public void add(int index, Object theElement);
     
-    /**
-     * Output the list elements from left to right
-     */
+    //output the list elements from left to right
     public String toString();
     
 }
